3f7fdd603744ccf6a08c518a2bbb93cec1a6eb41
Marat Bulatov <bulmarik@yandex.ru>
2021-05-05 02:56:12 +0700

    Развернуть сервер на Express

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const { PORT = 3000 } = process.env;
+
+
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+app.use(express.json());
+
+
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`)
+})
